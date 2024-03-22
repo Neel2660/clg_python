feeecf9f62a57956963799b2bcd8e0cef9c9bdf0
Neel2660 <145160286+Neel2660@users.noreply.github.com>
2024-03-22 14:59:52 +0530

    Create practical 1.py

---
+++ practical/practical 1.py
@@ -0,0 +1,6 @@
+# program to tell yow many years are left till you become 100ys old
+
+name = input("enter your name")
+age = int(input("enter your current age"))
+ans = 100 - age
+print(f"hey, {name} in {ans} years you will turn 100")
