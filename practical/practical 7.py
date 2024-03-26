a75cefd057dfc1859092a10e6dc6706641ad0edb
Neel2660 <145160286+Neel2660@users.noreply.github.com>
2024-03-26 13:52:30 +0530

    Create practical 7.py

---
+++ practical/practical 7.py
@@ -0,0 +1,9 @@
+# string = input("input your string: ")
+# str_len = len(string)
+# vov_no = 0
+# for i in string:
+#     if i.lower() == "a" or  i.lower() == "e" or i.lower() == "o" or i.lower() == "u" or  i.lower() == "i" :
+#         vov_no = vov_no + 1 
+# print("total no of vovel in your string are:",vov_no)
+# # print(str_len)
+# # print(string)
